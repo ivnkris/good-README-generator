88e972602e70bb1286b6024bbd4d585a5af23cbf
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-05-02 11:49:00 +0100

    added basic project info questions

--- index.js
+++ index.js
@@ -3,7 +3,34 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "list",
+    choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
+    name: "license",
+    message: "Select a license for your project",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the project's description?",
+  },
+  {
+    type: "confirm",
+    name: "installation",
+    message: "Are there any installation requirements?",
+  },
+  {
+    type: "input",
+    name: "installationCode",
+    message: "Type in the installation code directly here:",
+  },
+];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {};
